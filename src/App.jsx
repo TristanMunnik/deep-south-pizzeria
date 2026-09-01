bd8fba230aad9f8d86be499c39a7fe5a5693b375
Tristan Munnik <tristanmunnik3@gmail.com>
2026-09-01 11:19:52 +0200

    Added and styled header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Header from "./components/Header"
+
 function App() {
   return (
     <div>
-      <h1>Deep South Pizza Co.</h1>
-       <h1 className="text-4xl font-bold text-blue-400">Deep South Pizza Co.</h1>
-      <p>Fresh pizza, local flavor, family feel.</p>
+      <Header/>
     </div>
   )
 }
